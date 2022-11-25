b33cad65fbfb1b01469efac7879c3bcd60e16f8a
luckyluke023 <33722491+luckyluke023@users.noreply.github.com>
2022-11-25 14:24:34 +0800

    Update day5.py
    secrets things added

--- day5.py
+++ day5.py
@@ -10,6 +10,21 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
 st.header('st.write')
 
 st.write('Hello, *World!* :sunglasses:')
